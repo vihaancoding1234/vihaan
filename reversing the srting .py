9849be907e551f8d38959d9f2849c78c7765f612
vihaancoding1234 <aggvihaan.ktm@gmail.com>
2026-01-29 19:55:58 +0545

    Add files via upload

---
+++ reversing the srting .py
@@ -0,0 +1,6 @@
+string = input("please enter your own string :")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+print("\nThe orignal string = ",string)
+print("the revershed string = ",string2)
